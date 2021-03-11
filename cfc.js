a120f51bb711e4d6964cc3a9c61f9fae612c8fad
Aras14HD <jorimbk@gmail.com>
2021-03-11 19:00:51 +0100

    optimzed

--- cfc.js
+++ cfc.js
@@ -1,6 +1,8 @@
 const ChallengeFromCSV = {
   start: (e, userID, APIToken) => {
-    ChallengeFromCSV.fileParse(e).then(result => ChallengeFromCSV.sendData(result, userID, APIToken));
+    ChallengeFromCSV.fileParse(e).then((result) =>
+      ChallengeFromCSV.sendData(result, userID, APIToken)
+    );
   },
   /**
    *Sends the data via AJAX to habitica
@@ -9,58 +11,71 @@ const ChallengeFromCSV = {
    * @param {string} APIToken - APIToken
    */
   sendData: (data, userID, APIToken) => {
-    ChallengeFromCSV.postRequest("https://habitica.com/api/v3/challenges", userID, APIToken, data.Cdata).then(function(response) {
+    ChallengeFromCSV.postRequest(
+      "https://habitica.com/api/v3/challenges",
+      userID,
+      APIToken,
+      data.Cdata
+    ).then(function (response) {
       var id = JSON.parse(response).data.id;
-      for(const task of data.tArray) {
-        ChallengeFromCSV.postRequest("https://habitica.com/api/v3/tasks/challenge/" + id, userID, APIToken, task);
+      for (const task of data.tArray) {
+        ChallengeFromCSV.postRequest(
+          "https://habitica.com/api/v3/tasks/challenge/" + id,
+          userID,
+          APIToken,
+          task
+        );
       }
-    })
+    });
   },
   /**
-  *Sends POST request to specified url with authentication and parameters
+   *Sends POST request to specified url with authentication and parameters
    * @param {string} url - url to send POST request to
    * @param {string} userID - userID
    * @param {string} APIToken - APIToken
    * @param {Object} queryParams - Request Payload
    * @returns {Promise} Promise for the POST request
    */
-  postRequest: (url, userID, APIToken, queryParams={}) => {
-		let promise = new Promise((resolve, reject) => {
-			let req = new XMLHttpRequest();
-			req.open("POST", url);
+  postRequest: (url, userID, APIToken, queryParams = {}) => {
+    let promise = new Promise((resolve, reject) => {
+      let req = new XMLHttpRequest();
+      req.open("POST", url);
 
-			req.onerror = function() {
-				reject(this.responseText);
-			};
+      req.onerror = function () {
+        reject(this.responseText);
+      };
 
-			req.onload = function() {
-				if (req.status === 200 || 201) {
-					resolve(this.responseText);
-				} else {
-					reject(this.responseText);
-				}
-			}
+      req.onload = function () {
+        if (req.status === 200 || 201) {
+          resolve(this.responseText);
+        } else {
+          reject(this.responseText);
+        }
+      };
       req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-			req.setRequestHeader("x-client", "456b5feb-bd5c-4046-b5b3-83606a1f6a76-cfc");
-			req.setRequestHeader("x-api-user", userID);
-			req.setRequestHeader("x-api-key", APIToken);
-			req.send(JSON.stringify(queryParams));
-		});
-		return promise;
-	},
+      req.setRequestHeader(
+        "x-client",
+        "d2195ddc-a540-4dad-b3d5-34fb1ba8d319-cfc"
+      );
+      req.setRequestHeader("x-api-user", userID);
+      req.setRequestHeader("x-api-key", APIToken);
+      req.send(JSON.stringify(queryParams));
+    });
+    return promise;
+  },
   /**
    *Converts CSV/TXT File to usable data.
    * @param {Object} e - The Input Element of the CSV/TXT
    * @returns {Object} An object containing a challenge and an array containig the tasks.
    */
   fileParse: (e) => {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var FileList = e.files;
       if (FileList.length > 1) console.error("multiple files selected");
       var File = FileList[0];
       var reader = new FileReader();
       reader.readAsText(File);
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         Text = e.target.result;
         var tempArray = Text.split("\r\n");
         //Data for the challenge
@@ -70,12 +85,12 @@ const ChallengeFromCSV = {
           shortName: tempArray[1],
           summary: tempArray[2],
           description: tempArray[3],
-          prize: tempArray[5]
+          prize: tempArray[5],
         };
         //tasks
         var tArray = [];
         var task = false;
-        for(i = 6; i < tempArray.length; i++) {
+        for (i = 6; i < tempArray.length; i++) {
           var taskArray = tempArray[i].split(";");
           var tObject = {
             type: taskArray[0],
@@ -83,7 +98,7 @@ const ChallengeFromCSV = {
             notes: taskArray[2],
           };
           //task difficulty
-          switch(taskArray[3]) {
+          switch (taskArray[3]) {
             case "Trivial":
               var priority = 0.1;
               break;
@@ -97,86 +112,109 @@ const ChallengeFromCSV = {
               var priority = 2;
           }
           //task type
-          switch(taskArray[0]) {
+          switch (taskArray[0]) {
             case "habit":
-              tObject = Object.assign({
-                priority: priority,
-                startDate: taskArray[4]
-              }, tObject);
+              tObject = Object.assign(
+                {
+                  priority: priority,
+                  startDate: taskArray[4],
+                },
+                tObject
+              );
               break;
             case "daily":
-            tObject = Object.assign({
-              priority: priority,
-              frequency: taskArray[5],
-              startDate: taskArray[4]
-            }, tObject);
-            switch(taskArray[5]) {
-              //task daily type
-              case "daily":
-                tObject = Object.assign({
-                  everyX: taskArray[6]
-                }, tObject);
-                break;
-              case "weekly":
-                var days = taskArray[6].split(",");
-                var str = "{";
-                for (j = 0; j < days.length; j++) {
-                  if (days.length != 0) {
-                    str += "\"" + days[j] + "\":false";
-                    if (j != days.length - 1) str += ",";
-                  };
-                };
-                str += "}";
-                var tempObject = JSON.parse(str);
-                tObject = Object.assign({
-                  repeat: tempObject
-                }, tObject);
-                break;
-              case "monthly":
-                var weeks = taskArray[6].split(",");
-                var str = "{";
-                for (j = 0; j < weeks.length; j++) {
-                  if (weeks.length != 0) {
-                    str += "\"" + weeks[j] + "\":false";
-                    if (j != weeks.length - 1) str += ",";
+              tObject = Object.assign(
+                {
+                  priority: priority,
+                  frequency: taskArray[5],
+                  startDate: taskArray[4],
+                },
+                tObject
+              );
+              switch (taskArray[5]) {
+                //task daily type
+                case "daily":
+                  tObject = Object.assign(
+                    {
+                      everyX: taskArray[6],
+                    },
+                    tObject
+                  );
+                  break;
+                case "weekly":
+                  var days = taskArray[6].split(",");
+                  var str = "{";
+                  for (j = 0; j < days.length; j++) {
+                    if (days.length != 0) {
+                      str += '"' + days[j] + '":false';
+                      if (j != days.length - 1) str += ",";
+                    }
                   }
-                };
-                str += "}";
-                var tempObject = JSON.parse(str);
-                if (taskArray[7] == 0){
-                  var dom = taskArray[8].split(",");
-                  tObject = Object.assign({
-                    repeat: tempObject,
-                    daysOfMonth: dom
-                  }, tObject);
-                } else {
-                  var wom = taskArray[8].split(",");
-                  tObject = Object.assign({
-                    repeat: tempObject,
-                    weeksOfMonth: wom
-                  }, tObject);
-                }
-            }
-            break;
+                  str += "}";
+                  var tempObject = JSON.parse(str);
+                  tObject = Object.assign(
+                    {
+                      repeat: tempObject,
+                    },
+                    tObject
+                  );
+                  break;
+                case "monthly":
+                  var weeks = taskArray[6].split(",");
+                  var str = "{";
+                  for (j = 0; j < weeks.length; j++) {
+                    if (weeks.length != 0) {
+                      str += '"' + weeks[j] + '":false';
+                      if (j != weeks.length - 1) str += ",";
+                    }
+                  }
+                  str += "}";
+                  var tempObject = JSON.parse(str);
+                  if (taskArray[7] == 0) {
+                    var dom = taskArray[8].split(",");
+                    tObject = Object.assign(
+                      {
+                        repeat: tempObject,
+                        daysOfMonth: dom,
+                      },
+                      tObject
+                    );
+                  } else {
+                    var wom = taskArray[8].split(",");
+                    tObject = Object.assign(
+                      {
+                        repeat: tempObject,
+                        weeksOfMonth: wom,
+                      },
+                      tObject
+                    );
+                  }
+              }
+              break;
             case "todo":
-            tObject = Object.assign({
-              priority: priority,
-              date: taskArray[4]
-            }, tObject);
-            break;
+              tObject = Object.assign(
+                {
+                  priority: priority,
+                  date: taskArray[4],
+                },
+                tObject
+              );
+              break;
             case "reward":
-            tObject = Object.assign({
-              value: taskArray[3]
-            }, tObject);
-          };
+              tObject = Object.assign(
+                {
+                  value: taskArray[3],
+                },
+                tObject
+              );
+          }
           tArray.push(tObject);
         }
         resolve({
           Cdata: Cdata,
-          tArray: tArray
+          tArray: tArray,
         });
-      }
+      };
     });
-
-  }
-}
+  },
+};
--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-  if (localStorage.getItem('userID') != null) {
-    document.getElementById("UserID").value = localStorage.getItem('userID');
-    document.getElementById("APItoken").value = localStorage.getItem('APIToken');
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("userID") != null) {
+    document.getElementById("UserID").value = localStorage.getItem("userID");
+    document.getElementById("APItoken").value = localStorage.getItem(
+      "APIToken"
+    );
   }
-  document.getElementById("challengeFile").addEventListener('change', function() {
-    ChallengeFromCSV.fileParse(this).then(response => displayData(response))
-  }, false)
-  document.getElementById("Submit").addEventListener("click", function() {
+  document.getElementById("challengeFile").addEventListener(
+    "change",
+    function () {
+      ChallengeFromCSV.fileParse(this).then((response) =>
+        displayData(response)
+      );
+    },
+    false
+  );
+  document.getElementById("Submit").addEventListener("click", function () {
     userID = document.getElementById("UserID").value;
     APIToken = document.getElementById("APItoken").value;
-    localStorage.setItem('userID', userID);
-    localStorage.setItem('APIToken', APIToken);
-    ChallengeFromCSV.sendData(JSON.parse(sessionStorage.getItem('data')), userID, APIToken)
-  })
-})
+    localStorage.setItem("userID", userID);
+    localStorage.setItem("APIToken", APIToken);
+    ChallengeFromCSV.sendData(
+      JSON.parse(sessionStorage.getItem("data")),
+      userID,
+      APIToken
+    );
+  });
+});
 function displayData(response) {
   var userID = document.getElementById("UserID").value;
   var APIToken = document.getElementById("APItoken").value;
-  sessionStorage.setItem('data', JSON.stringify(response));
+  sessionStorage.setItem("data", JSON.stringify(response));
   var Cdata = response.Cdata;
   var tArray = response.tArray;
   var html = "<div id='Cdata'>\n<h1>Challenge:</h1>\n";
-  html += "<h2>" + Cdata.name + "</h2>"
-  getRequest("https://habitica.com/api/v3/groups/" + Cdata.group, userID, APIToken).then(function(response) {
-    console.log(JSON.parse(response).data)
-    html += "group: " + Cdata.group + "(" + JSON.parse(response).data.name + ")<br/>";
+  html += "<h2>" + Cdata.name + "</h2>";
+  getRequest(
+    "https://habitica.com/api/v3/groups/" + Cdata.group,
+    userID,
+    APIToken
+  ).then(function (response) {
+    console.log(JSON.parse(response).data);
+    html +=
+      "group: " + Cdata.group + "(" + JSON.parse(response).data.name + ")<br/>";
     for (const [key, value] of Object.entries(Cdata)) {
-      if (key !== "name" && key !== "group") html += key + ": " + value + "<br/>";
+      if (key !== "name" && key !== "group")
+        html += key + ": " + value + "<br/>";
     }
-    html += "</div>\n<div id='tArray'>\n<h2>Tasks:</h2>"
-    for (i =  0; i < tArray.length; i++) {
-      html += "<h3>" + tArray[i].text + ":</h3>"
+    html += "</div>\n<div id='tArray'>\n<h2>Tasks:</h2>";
+    for (i = 0; i < tArray.length; i++) {
+      html += "<h3>" + tArray[i].text + ":</h3>";
       for (const [key, value] of Object.entries(tArray[i])) {
         if (key !== "text") html += key + ": " + value + "<br/>";
       }
     }
     document.getElementById("Data").innerHTML = html;
-  })
-
+  });
 }
 
 /**
@@ -50,25 +66,27 @@ function displayData(response) {
  * @returns {Promise<String>} Promise containing the raw API response data as a string.
  */
 function getRequest(url, userID, APIToken) {
-
   let promise = new Promise((resolve, reject) => {
     let req = new XMLHttpRequest();
     req.open("GET", url);
 
-    req.onerror = function() {
+    req.onerror = function () {
       reject(this.statusText);
     };
 
-    req.onload = function() {
+    req.onload = function () {
       if (this.status == 200) {
         resolve(this.responseText);
       } else {
         reject(this.responseText);
       }
-    }
+    };
     req.setRequestHeader("x-api-user", userID);
     req.setRequestHeader("x-api-key", APIToken);
-    req.setRequestHeader("x-client", "456b5feb-bd5c-4046-b5b3-83606a1f6a76-cfc");
+    req.setRequestHeader(
+      "x-client",
+      "456b5feb-bd5c-4046-b5b3-83606a1f6a76-cfc"
+    );
     req.send();
   });
 
