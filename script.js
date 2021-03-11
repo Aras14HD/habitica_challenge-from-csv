155c7657be78208c0805e02b69b68c6f46d32bab
Aras14HD <jorimbk@gmail.com>
2021-03-11 10:36:09 +0100

    improved data display

--- script.js
+++ script.js
@@ -5,23 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("APItoken").value = localStorage.getItem('APIToken');
   }
   document.getElementById("challengeFile").addEventListener('change', function() {
-    ChallengeFromCSV.fileParse(this).then(function(response) {
-      sessionStorage.setItem('data', JSON.stringify(response));
-      Cdata = response.Cdata;
-      tArray = response.tArray;
-      html = "<div id='Cdata'>\n<h2>Cdata:</h2>\n";
-      for (const [key, value] of Object.entries(Cdata)) {
-        html += key + ": " + value + "<br/>";
-      }
-      html += "</div>\n<div id='tArray'>\n<h2>tArray:</h2>"
-      for (i =  0; i < tArray.length; i++) {
-        html += "<h3>Task" + i + ":</h3>"
-        for (const [key, value] of Object.entries(tArray[i])) {
-          html += key + ": " + value + "<br/>";
-        }
-      }
-      document.getElementById("Data").innerHTML = html;
-    })
+    ChallengeFromCSV.fileParse(this).then(response => displayData(response))
   }, false)
   document.getElementById("Submit").addEventListener("click", function() {
     userID = document.getElementById("UserID").value;
@@ -31,3 +15,62 @@ document.addEventListener("DOMContentLoaded", function() {
     ChallengeFromCSV.sendData(JSON.parse(sessionStorage.getItem('data')), userID, APIToken)
   })
 })
+function displayData(response) {
+  userID = document.getElementById("UserID").value;
+  APIToken = document.getElementById("APItoken").value;
+  sessionStorage.setItem('data', JSON.stringify(response));
+  Cdata = response.Cdata;
+  tArray = response.tArray;
+  html = "<div id='Cdata'>\n<h1>Challenge:</h1>\n";
+  html += "<h2>" + Cdata.name + "</h2>"
+  getRequest("https://habitica.com/api/v3/groups/" + Cdata.group, userID, APIToken).then(function(response) {
+    console.log(JSON.parse(response).data)
+    html += "group: " + Cdata.group + "(" + JSON.parse(response).data.name + ")<br/>";
+    for (const [key, value] of Object.entries(Cdata)) {
+      if (key !== "name" && key !== "group") html += key + ": " + value + "<br/>";
+    }
+    html += "</div>\n<div id='tArray'>\n<h2>Tasks:</h2>"
+    for (i =  0; i < tArray.length; i++) {
+      html += "<h3>" + tArray[i].text + ":</h3>"
+      for (const [key, value] of Object.entries(tArray[i])) {
+        if (key !== "text") html += key + ": " + value + "<br/>";
+      }
+    }
+    document.getElementById("Data").innerHTML = html;
+  })
+
+}
+
+/**
+ * Make a GET request to the Habitica API.
+ * Data object returned varies based on the API url called.
+ * For accessing personal data endpoints, use {@link HabiticaAPIManager#authGetRequest|authGetRequest}
+ * @param {string} baseURL - the url of the api call.
+ * @param {object} [queryParams={}] - key-value pairs for any parameters needed by the api call.
+ * @returns {Promise<String>} Promise containing the raw API response data as a string.
+ */
+function getRequest(url, userID, APIToken) {
+
+  let promise = new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open("GET", url);
+
+    req.onerror = function() {
+      reject(this.statusText);
+    };
+
+    req.onload = function() {
+      if (this.status == 200) {
+        resolve(this.responseText);
+      } else {
+        reject(this.responseText);
+      }
+    }
+    req.setRequestHeader("x-api-user", userID);
+    req.setRequestHeader("x-api-key", APIToken);
+    req.setRequestHeader("x-client", "456b5feb-bd5c-4046-b5b3-83606a1f6a76-cfc");
+    req.send();
+  });
+
+  return promise;
+}
